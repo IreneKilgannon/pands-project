3a0e211d816345fd7560272d1d3afbf36e33d35f
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-14 11:12:32 +0000

    import numpy

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -6,9 +7,12 @@ iris = pd.read_csv("iris.data")
 # Having a look at the data set, checking that it loaded. 
 print(iris.head())
 
+# Add column names
+iris.columns = ['sepal_length_cm', 'sepal_width_cm', 'petal_length_cm', 'petal_width_cm', 'class']
+print(iris.head())
+
 # Summary of the variables and data types in the data set
 print(iris.info())
-
 
 # Summary statistics of the data set
 print(iris.describe())
