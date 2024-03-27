7c6cf3820bd600531b3c0bcb8fd7c3157df4063f
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-27 13:44:56 +0000

    adding title, labelling axis

--- analysis.py
+++ analysis.py
@@ -41,16 +41,20 @@ print(f'Summary statistics for Iris virginica are: \n{virginica.describe()}')
 print()
 
 # Histogram of Sepal Length by species. 
-fig, ax = plt.subplots(1, 3, sharex= True, sharey= True)
-ax[0].hist(setosa['sepal_length_cm'], label = 'Iris setosa', edgecolor = 'black', alpha = 0.3, bins = 15)
-ax[1].hist(versicolor['sepal_length_cm'], label = 'Iris versicolor', edgecolor = 'black', alpha = 0.3, bins = 15)
-ax[2].hist(virginica['sepal_length_cm'], label = 'Iris virginica', edgecolor = 'black', alpha = 0.3, bins = 15)
-#ax.suptitle('Histogram of sepal length for Iris species')
-#ax.set_xlabel('Sepal length (cm)')
-#ax.set_ylabel('No of occurrances')
-#ax.legend()
+fig, ax = plt.subplots(1, 3, sharex= True, sharey= True, figsize = (8, 6))
+ax[0].hist(setosa['sepal_length_cm'], edgecolor = 'black', alpha = 1.0, bins = 15)
+ax[1].hist(versicolor['sepal_length_cm'], label = 'Iris versicolor', color = 'orange', edgecolor = 'black', alpha = 1.0, bins = 15)
+ax[2].hist(virginica['sepal_length_cm'], label = 'Iris virginica', color = 'green', edgecolor = 'black', alpha = 1.0, bins = 15)
+
+ax[0].set_xlabel('Sepal length (cm) \nIris setosa')
+ax[1].set_xlabel('Sepal length (cm), \nIris versicolor')
+ax[2].set_xlabel('Sepal length (cm), \nIris virginica')
+ax[0].set_ylabel('No of instances')
+
+fig.suptitle('Histogram of Sepal Length by Iris species')
+ax[0].legend('setosa')
 plt.savefig('sepal_length.png')
-plt.show()
+#plt.show()
 
 def iris_hist(**kwargs):
     '''A function that accepts user inputs to plot a histogram for the iris data set'''
