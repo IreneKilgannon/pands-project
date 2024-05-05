c8768becfe0a34fc4d8ec7137824aaef8ddfc059
IreneKilgannon <irene.kilgannon@gmail.com>
2024-05-05 12:02:56 +0100

    changed variable names of hist and scatter function, added comments to py file, other random stuff

--- analysis.py
+++ analysis.py
@@ -3,39 +3,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-iris = pd.read_csv("iris.data", header = None)
+# Setting up seaborn palette of 'Set1'
+sns.set_palette("Set1")
 
-# Having a look at the data set, checking that it loaded. 
+# Import the data set, add headings to the columns.
+iris = pd.read_csv("iris_data.csv", names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'])
+
+# Check that the data set has loaded. 
 head = f'The first five rows of the data set are: \n {iris.head()}\n \n'
 
+# Open and write the head of the data set to analysis.txt.
 with open('analysis.txt', 'wt') as f:
     f.write(head)
 
-# Add column names
-iris.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
-add_header = f'Checking that the column names are correct: \n {iris.head()}\n \n'
+#### Output a summary of each variable to a single txt file, analysis.txt ####
 
-with open('analysis.txt', 'a') as f:
-    f.write(add_header)
+# Collating the necessary infomation for analysis.txt
 
-# Summary of the variables and data types in the data set
-info = f'Summary of the variables and the data types in the data set {iris.info()} \n\n'
+# Get the number of rows and columns in the data set.
+shape = f'The shape of the data set is {iris.shape}. \n\n'
 
+# Get the variable names.
+column_names = f'Summary of the variable names in the data set are: \n {iris.columns} \n\n'
+
+# Get the data types of the variables.
 data_types = f'The data types in the data set are: \n{iris.dtypes}\n \n'
 
-# Looking for missing data, NaN
+# Look for missing data, NaN
 missing_values = f'Checking to see if there is any missing data or NaN. \n{iris.isna().sum()} \n \n'
 
-
-# Uniques names of flower species
+# Uniques names in the species column.
 unique = f"The unique names in the species column are: \n {iris['species'].unique()} \n\n"
 
-
-# Summary statistics of the data set
+# Summary statistics for the overall the data set
 summary_statistics = f'Overall summary statistics for the data set. \n{iris.describe()} \n\n'
 
-
-# Creating dataframes for each iris species.
+# Create dataframes for each iris species.
 setosa = iris[iris['species'] == 'Iris-setosa']
 versicolor = iris[iris['species'] == 'Iris-versicolor']
 virginica = iris[iris['species'] == 'Iris-virginica']
@@ -44,17 +47,18 @@ virginica = iris[iris['species'] == 'Iris-virginica']
 # Summary Statistics for Iris setosa
 setosa_summary = f'Summary statistics for Iris setosa are: \n{setosa.describe()} \n\n'
 
-
 # Summary Statistics for Iris versicolor
 versicolor_summary = f'Summary statistics for Iris versicolor are: \n{versicolor.describe()} \n\n'
-
 
 # Summary Statistics for Iris virginica
 virginica_summary = f'Summary statistics for Iris virginica are: \n{virginica.describe()} \n\n'
 
-# Write summary statistics to analysis.txt
+# Write the summary statistics to analysis.txt
+# Append them to the analysis.txt file created previously
+
 with open('analysis.txt', 'a') as f:
-    f.write(info)
+    f.write(shape)
+    f.write(column_names)
     f.write(data_types)
     f.write(missing_values)
     f.write(unique)
@@ -63,78 +67,115 @@ with open('analysis.txt', 'a') as f:
     f.write(versicolor_summary)
     f.write(virginica_summary)
 
-sns.set_palette("Set1")
 
-# Comparing summary statistics across the three species.
-fig, axes = plt.subplots(2,2, figsize = (10, 10))
-sns.boxplot(ax = axes[0, 0], x = 'species', y = 'sepal_length', data = iris)
-sns.boxplot(ax = axes[0, 1], x = 'species', y= 'sepal_width', data = iris)
-sns.boxplot(ax = axes[1, 0], x = 'species', y = 'petal_length', data = iris)
-sns.boxplot(ax = axes[1, 1], x = 'species', y = 'petal_width', data = iris)
+# Create a box plot to visually compare the summary statistics across the three species in the data set.
+
+# Create a fig, ax plot
+fig, ax = plt.subplots(2,2, figsize = (10, 10))
+
+# Create a box plot for each variable, coloured by species.
+sns.boxplot(ax = ax[0, 0], x = 'species', y = 'sepal_length', data = iris)
+sns.boxplot(ax = ax[0, 1], x = 'species', y= 'sepal_width', data = iris)
+sns.boxplot(ax = ax[1, 0], x = 'species', y = 'petal_length', data = iris)
+sns.boxplot(ax = ax[1, 1], x = 'species', y = 'petal_width', data = iris)
+
+# Overall plot title
 plt.suptitle('Box plot by Species for Each Variable')
-axes[0,0].set_title('Sepal Length')
-axes[0,1].set_title('Sepal Width')
-axes[1,0].set_title('Petal Length')
-axes[1,1].set_title('Petal Width')
-plt.savefig('C:\\Users\\Martin\\Desktop\\pands\\pands-project\\plots\\Boxplot.png')
+
+# Label each plot
+ax[0,0].set_title('Sepal Length')
+ax[0,1].set_title('Sepal Width')
+ax[1,0].set_title('Petal Length')
+ax[1,1].set_title('Petal Width')
+
+# Save the created plot
+plt.savefig('C:\\Users\\Martin\\Desktop\\pands\\pands-project\\plots\\Box_plot.png')
 plt.close()
 
-#### Histogram #####
+#### Save a histogram of each variable to png files #####
 
-# Save a histogram of each variable to png files
+# Save a histogram of each variable to png files.
+# Creating a function that iterateds through the data set, would ideally like to generalise more so that it could in theory be used for any data set.
+
 def plot_hist(df):
-    for col in df:
+    for x in df:
         #sns.set_palette("Set1")
-        sns.histplot(x = col, data = df, hue = 'species')
-        plt.title(f"Histogram of {col.title().replace('_', ' ')}")
-        plt.xlabel(f"{col.replace('_', ' ')}")
-        plt.savefig(f'C:\\Users\\Martin\\Desktop\\pands\\pands-project\\plots\\Histogram_of_{col}.png')
+        sns.histplot(x = x, data = df, hue = 'species')
+        plt.title(f"Histogram of {x.title().replace('_', ' ')}")
+        plt.xlabel(f"{x.replace('_', ' ')}")
+        plt.savefig(f'C:\\Users\\Martin\\Desktop\\pands\\pands-project\\plots\\Histogram_of_{x}.png')
         #plt.show()
         plt.close()
 
 plot_hist(iris)
 
-#### Scatter plot of each pair of variables
-fig, ax = plt.subplots()
+#### Output a scatter plot of each pair of variables ####
 
-for col1 in iris:
-    for col2 in iris:
-        if col1 == col2 or col1 == 'species' or col2 == 'species':
-            continue
-        else:
-            sns.scatterplot(data = iris, x = col1, y = col2, hue = 'species')
-            plt.title(f"Scatter plot of {col2.title().replace('_', ' ')} vs {col1.title().replace('_', ' ')}")
-            plt.xlabel(f"{col1.title().replace('_', ' ')}")
-            plt.ylabel(f"{col2.title().replace('_', ' ')}")
-            plt.savefig(f'C:\\Users\\Martin\\Desktop\\pands\\pands-project\\plots\\Scatterplot_{col2}_vs_{col1}.png')
-            plt.close()
+# Creating a function
 
-# Outputs a scatter plot of each pair of variables
-sns.pairplot(iris, hue = 'species')
-plt.suptitle('Pair Plot of the Numeric Variables in the Iris Data Set', y = 1.05)
+# How would I improve it, 
+    # 1. generalise it more, if x or y are an object data type, skip 
+    # 2. only 1 of plot of each eg currently getting petal length vs petal width and then petal width vs petal length. One plot would do.
+    # 
+
+def plot_scatter(df):
+    for x in df:
+        for y in df:
+            # Do not create a scatter plot for the following conditions
+            if x == y or x == 'species' or y == 'species':
+                continue
+            else:
+                # Create a scatter plot
+                sns.scatterplot(data = df, x = x, y = y, hue = 'species')
+
+                # Add title to plot
+                plt.title(f"Scatter plot of {y.title().replace('_', ' ')} vs {x.title().replace('_', ' ')}")
+
+                # Label x and y-axis
+                plt.xlabel(f"{x.title().replace('_', ' ')}")
+                plt.ylabel(f"{y.title().replace('_', ' ')}")
+
+                # Save a scatter plot for each pair of variables
+                plt.savefig(f'C:\\Users\\Martin\\Desktop\\pands\\pands-project\\plots\\Scatterplot_{y}_vs_{x}.png')
+                plt.close()
+
+plot_scatter(iris)
+
+# Use a pair plot! Much simplier method to generate a scatter plot of each pair of variables
+g = sns.pairplot(iris, hue = 'species')
+g.fig.suptitle('Pair Plot of the Numeric Variables in the Iris Data Set', y = 1.05)
 plt.savefig('C:\\Users\\Martin\\Desktop\\pands\\pands-project\\plots\\Pair_plot.png')
+plt.close
 #plt.show()
 
 
-## Any other analysis 
+#### Any other analysis ####
+
+# Create a correlation matrix between the numeric variables in the data set.
+correlation_matrix = iris.drop(['species'], axis = 1).corr()
 
 
-iris.drop(['species'], axis = 1).corr()
-plt.savefig('C:\\Users\\Martin\\Desktop\\pands\\pands-project\\plots\\Correlation_Matrix.png')
 
-
-# Heatmap of the Correlation Coefficients
+# Create a heatmap of the correlation coefficients between the variables in the data set.
 fig, ax = plt.subplots(2, 2, figsize = (12, 10))
 
+# Overall values  - not taking the flower species into account
 sns.heatmap(iris.drop(['species'], axis = 1).corr(), annot = True, ax = ax[0, 0], vmin = -0.5, vmax=1)
 ax[0,0].set_title('Overall')
+
+# Iris setosa
 sns.heatmap(setosa.drop(['species'], axis = 1).corr(), annot = True, ax = ax[0, 1], vmin = -0.5, vmax=1)
 ax[0,1].set_title('Iris setosa')
+
+# Iris versicolor
 sns.heatmap(versicolor.drop(['species'], axis = 1).corr(), annot = True, ax = ax[1, 0], vmin = -0.5, vmax=1)
 ax[1,0].set_title('Iris versicolor')
+
+# Iris virginica
 sns.heatmap(virginica.drop(['species'], axis = 1).corr(), annot = True, ax = ax[1,1], vmin = -0.5, vmax=1)
 ax[1,1].set_title('Iris virginica')
 
-
+# Add title
 plt.suptitle('Correlation Coefficients for the Iris Data Set')
 plt.savefig('C:\\Users\\Martin\\Desktop\\pands\\pands-project\\plots\\Heatmap_correlation_coefficients.png')
+plt.close()
